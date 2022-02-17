9f2ef895a270dd291953f113bf1a0f71e3537865
kanieksuke <95512847+kanieksuke@users.noreply.github.com>
2022-02-17 20:35:45 +0900

    createアクションを定義(targets)

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -2,4 +2,17 @@ class TargetsController < ApplicationController
   def new
     @target = Target.new
   end
+
+  def create
+    @target = Target.new(target_params)
+    @target.current_hp = @target.max_hp
+    @target.current_mp = 0
+    @target.save
+    redirect_to new_target_budget_path(@target.id)
+  end
+
+  private
+  def target_params
+    params.require(:target).permit(:max_hp, :max_mp).merge(user_id: current_user.id)
+  end
 end
